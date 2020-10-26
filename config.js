60218f3043fd17e0c47e1176783d6183d1d39032
Liam <liam@nehoray.net>
2020-10-26 15:30:26 +0100

    ping bot

---
+++ config.js
@@ -0,0 +1,17 @@
+const config = {
+
+  "prefix": ";", //Add your prefix here
+
+  "ownerOnly": false, //If set to true, only bot owner can control Pinger. If set to false, everyone can use Pinger.
+
+  "ownerID": "402885145566248960", //Id of bot owner for "ownerOnly" function.
+
+  "channelName": "pings", //Name of channel, where will be all pings sent.
+
+  "pingInterval": "5000", //Time in ms (miliseconds). 1000 recommended - If 1000ms (1s), bot will send ping every 1000ms (1s).
+
+  "token": "NzcwMjkwMTA3OTI3MjMyNTQy.X5bakQ.QLv0amzZyP72D_JZEa4lAfx9fXU" //Bot token from discord app.
+
+}
+
+module.exports = config;
